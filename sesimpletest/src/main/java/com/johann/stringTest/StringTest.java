be480ac8d1e98aa59e33f2cf5555ec16dc9bd974
yhZhao <yhzhaohy@163.com>
2019-11-04 18:14:15 +0800

    1104

--- sesimpletest/src/main/java/com/johann/stringTest/StringTest.java
+++ sesimpletest/src/main/java/com/johann/stringTest/StringTest.java
@@ -10,41 +10,32 @@ public class StringTest {
 
     public static void main(String[] args) {
 
-        String em = new String();
-        String em1 = "";
-        em = em.intern();
 
-        String s1 = new String("cat");
-        String s4 = s1.intern();
-        String s5 = new String("cat");
+        //在堆中创建一个 “hello”
+        String s3 = new String("hel") + new String("lo");
 
+        //在字符串池中创建一个 “hello”
+        String s4 = "hello";
 
-        String s2 = "cat";
-        String s10 = new String(s2);
+        //判断字符串池中是否存在“hello”,如果存在，直接返回字符串池中的对象；如果字符串池中的不存在，则把堆中的字符串对象添加到字符串池中
+        String s5 = s3.intern();
+        System.out.println(s5 == s3);
+        System.out.println(s5 == s4);
+        System.out.println(s3 == s4);
 
-        String s3 = "cat";
-        System.out.println(s3==s4);
+        System.out.println("======================");
 
+        //在堆中新建一个“good”
+        String s6 = new String("go") +new String("od");
 
-        System.out.println(s1==s2);
-        System.out.println(s2==s3);
-        System.out.println(s1==s5);
-        System.out.println(s4==s5);
+        //判断字符串池中是否存在“good”,如果存在，直接返回字符串池中的对象；如果字符串池中的不存在，则把堆中的字符串对象添加到字符串池中（注意不是copy一份，而是move）
+        String s7 = s6.intern();
 
-        String s6 = s1;
-        System.out.println(s6==s1);
-        System.out.println(s6==s2);
-
-        s1 = s1.intern();
-        System.out.println(s1==s2);
-        System.out.println(s6==s1);
-
-        String s7 = s1;
-        System.out.println(s7==s1);
-        System.out.println(s7==s2);
-        System.out.println(s6==s7);
-
-        //new StringTest().foo("");
+        //判断字符串池中是否存在“good”，若不存在则新建一个“good”；若存在，直接返回这个“good”对象
+        String s8 = "good";
+        System.out.println(s6 == s7);
+        System.out.println(s7 == s8);
+        System.out.println(s6 == s8);
     }
 
     public void foo(String s) {
